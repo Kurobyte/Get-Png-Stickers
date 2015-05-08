3da0441ac0e3b21b538719576394fc1567ebee9e
Ivan Morales <kurobyte@gmail.com>
2015-05-08 10:01:01 +0200

    Get Stickers(Python) v0.2 Added args in command line added a config file Added an option to download the productVersions.meta in order to check the version of the sticker(Not sure if it's necessary at all). The last productVersion.meta is at the version: `27490` (Look at the config file) If you force download the latest pV.meta the config will be overwrite with the last version ID. Keep in mind that the force download will try to download each versión til he reach the latest, so it can take a while if the DB versión is a bit outdated.

--- README.md
+++ README.md
@@ -14,15 +14,23 @@ https:// store.line.me/stickershop/product/***2677***/es
 that's the sticker ID
 
 ## Linux/Mac/Windows ##
+![](https://pbs.twimg.com/media/CEbc0CgWYAE95wq.png:large)
 For Linux/MAC/Windows you need python(2.7 or higher) installed in order to use the script.
 
 Open a command line and type
-> python sticker.py {sticker_id}
+> python sticker.py -s {sticker_id}
 
 Example:
-> python sticker.py 2677
+> python sticker.py -s 2677
 
-The stickers will be downloaded in the same dir as the script. If you want to download into another dir you SHOULD change the constant `OUTPUT_DIR` with the path you want.
+The stickers will be downloaded in the same dir as the script. If you want to download into another dir you SHOULD change the config parametrer `outdir` in `sticker.cfg` with the path you want(No '' neded).
+
+
+Updating the productVersion.meta
+> python sticker.py -p
+
+The last productVersion.meta is at the version: `27490` (Is the last at todays date[8 of may 2015] / Look at the config file) If you force download the latest pV.meta the config will be overwrite with the last version ID.
+Keep in mind that the force download will try to download each versión til he reach the latest, so it can take a while if the DB versión is a bit outdated.
 
 ## Only Windows ##
 For Windows you need the .NET Framework 4.0 installed.
--- sticker.py
+++ sticker.py
@@ -1,12 +1,45 @@
-import urllib
-import json
-import sys, os
+import urllib, json
+import sys, os, argparse, ConfigParser
+
+parser = argparse.ArgumentParser(description='Get Stickers developed by Kurobyte');
+parser.add_argument('-v', '--version', action='version', version='0.2');
+parser.add_argument('-p', '--productVersion', dest='prodVer', action='store_true', help='Preforms the download of the last productVersion.meta');
+parser.add_argument('-s', '--sticker_id', dest='sticker_id', action='store', help='Id of the sticker that you want to download');
+parser.add_argument('-t', '--tabs', dest='tabs', action='store_true', help='Use -t when you want to download the tab_on/off.png and preview.png');
+parser.add_argument('-i', '--ignore', dest='ignore', action='store_true', help='Ignore the local DB. Could fail the download.');
+args = parser.parse_args();
+
+config = ConfigParser.ConfigParser()
+config.readfp(open('sticker.cfg'))
 
 BASE_URL = 'http://dl.stickershop.line.naver.jp';
-OUTPUT_DIR = './';
+OUTPUT_DIR = config.get('user', 'outDir');
 
-def main(sticker_id):
-	comp = getMeta(sticker_id); 
+def main(sticker_id, tabs, ignore, version):
+	ver = 1
+	if not ignore:
+		f = None;
+		if os.path.isfile('./productVersions.meta'):
+			f = open('./productVersions.meta');
+		else :
+			downloadProductVersion(version);
+			f = open('./productVersions.meta');
+
+		sticVer = json.loads(f.read());
+		trobat = False;
+		i = 0;
+
+		while len(sticVer['versions']) > i and not trobat:
+			if int(sticker_id) in sticVer['versions'][i]:
+				ver = sticVer['versions'][i][1];
+				trobat = True;
+
+			i = i +1;
+
+		if not trobat:
+			print 'The id isn\'t in the local DB. If the download fails use the [-p] to fetch the newest DB(It could take a bit)';
+
+	comp = getMeta(sticker_id, ver); 
 	meta = comp[1];
 	metaStr = comp[0];
 	if meta != None:
@@ -21,18 +54,25 @@ def main(sticker_id):
 		print str(nStickers)+' Stickers';
 		for i in xrange(0, nStickers):
 			png_id = meta['stickers'][i]['id'];
-			downloadSticker(sticker_id, png_id, dirName)
+			downloadSticker(sticker_id, ver, png_id, dirName)
 			print "[%3.2f%%]" % ((i * 100) / (nStickers -1))," Downloaded         \r",
 
 		print "";
+		if tabs:
+			print "Downloading tabs";
+			downloadExtra(sticker_id, ver, 'LINEStorePC', 'main', dirName, 0);
+			downloadExtra(sticker_id, ver, 'android', 'tab_on', dirName, 0);
+			downloadExtra(sticker_id, ver, 'android', 'tab_off', dirName, 0);
+			downloadExtra(sticker_id, ver, 'LINEStorePC', 'preview', dirName, 0);
+
 		print "Download complete";
 		writeMeta(metaStr, dirName+'/productInfo.meta');
 
-def getMeta(sticker_id):
+def getMeta(sticker_id, ver):
 	meta = None;
 	metaStr = None;
 	try:
-		url = urllib.urlopen(BASE_URL+'/products/0/0/1/'+str(sticker_id)+'/android/productInfo.meta');
+		url = urllib.urlopen(BASE_URL+'/products/0/0/'+str(ver)+'/'+str(sticker_id)+'/android/productInfo.meta');
 		metaStr = url.read();
 		meta = json.loads( metaStr );
 	except Exception, e:
@@ -46,12 +86,38 @@ def writeMeta(meta, metaFile):
 	f.write(meta);
 	f.close();
 
-def downloadSticker(sticker_id, png_id, dirName):
+def downloadSticker(sticker_id, ver, png_id, dirName):
 	try:
-		urllib.urlretrieve(BASE_URL+'/products/0/0/1/'+str(sticker_id)+'/android/stickers/'+str(png_id)+'.png', OUTPUT_DIR+str(dirName)+'/'+str(png_id)+'.png');
+		urllib.urlretrieve(BASE_URL+'/products/0/0/'+str(ver)+'/'+str(sticker_id)+'/android/stickers/'+str(png_id)+'.png', OUTPUT_DIR+str(dirName)+'/'+str(png_id)+'.png');
 	except Exception, e:
 		print 'Error downloading the sticker: '+str(png_id);
 
+def downloadExtra(sticker_id, ver, source, name, dirName, inmer):
+	try:
+		urllib.urlretrieve(BASE_URL+'/products/0/0/'+str(ver)+'/'+str(sticker_id)+'/'+source+'/'+name+'.png', OUTPUT_DIR+str(dirName)+'/'+name+'.png');
+	except Exception, e:
+		if inmer == 0:
+			print 'Error downloading: '+name+'\r\nTrying to download from other source';
+			downloadExtra(sticker_id, ver, ('android' if source == 'LINEStorePC' else 'iphone' ) , name, dirName, 1);
+
+def forceUpgradeProductVersion(version):
+	try:
+		urllib.urlopen(BASE_URL+'/products/productVersions_'+str(version)+'.meta');
+		version = forceUpgradeProductVersion(version + 1);
+	except Exception, e:
+		version = version - 1;
+		downloadProductVersion(version);
+
+	return version;
+
+def downloadProductVersion(version):
+	if os.path.isfile('./productVersions.meta'):
+		os.remove('./productVersions.meta');
+
+	try:
+		urllib.urlretrieve(BASE_URL+'/products/productVersions_'+str(version)+'.meta', './productVersions.meta');
+	except Exception, e:
+		print 'productVersion.meta couldn\'t be done.';
 
 def replace_all(text):
 	nonValid = ['<', '>', ':', '"', '\\', '/', '|', '?', '*'];
@@ -61,7 +127,11 @@ def replace_all(text):
 
 	return text
 
-if len(sys.argv) > 1:
-	main(sys.argv[1]);
-else:
-	print "You should provide a sticker id in order to download'it.";
+if args.prodVer:
+	latest = forceUpgradeProductVersion(int(config.get('aplication', 'prodVer')));
+	config.set('aplication', 'prodVer', latest);
+	config.write(open('sticker.cfg', 'wb'));
+if args.sticker_id:
+	main(args.sticker_id, args.tabs, args.ignore, int(config.get('aplication', 'prodVer')));
+else :
+	print "You should provide a sticker id with the param [-s].";
